8958041260834bf9ca80acfae9590fee18ba898f
EC2 Default User <ec2-user@ip-172-31-3-41.ec2.internal>
2020-10-03 10:05:34 +0000

    [応用課題３いいね機能リダイレクト対応]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :set_favo
+
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path
+    redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to books_path
+    redirect_to request.referer
+  end
+
+  private
+  def set_favo
+    @book = Book.find(params[:book_id])
   end
 end
