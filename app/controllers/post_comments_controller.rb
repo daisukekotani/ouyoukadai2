157df02c514bce7398ba86eef661c2f32e9bbc67
EC2 Default User <ec2-user@ip-172-31-3-41.ec2.internal>
2020-10-04 08:45:45 +0000

    [Finish]応用課題３

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,13 +1,16 @@
 class PostCommentsController < ApplicationController
-   def create
+
+  def create
     book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = book.id
+    @post_comment.save
     redirect_to request.referer
-   end
+  end
 
   def destroy
+    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to request.referer
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   devise_for :users
+  devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
   resources :books do
   resource :favorites, only: [:create, :destroy]
